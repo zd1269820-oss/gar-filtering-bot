f3f7ebb798c242e84c9962b8ca5f68996f02c99f
zd1269820-oss <zd1269820@gmail.com>
2026-01-29 19:02:27 -0500

    Delete commands directory

--- commands/package.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Check if the bot is alive",
-
-  async execute(interaction) {
-    await interaction.reply("✅ Sentinel Bot is online!");
-  }
-};
